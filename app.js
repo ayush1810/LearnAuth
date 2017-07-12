882f2ba0637f9c86b536851d6fb517a49c03e55e
Ayush Agrawal <ayushthetechie@gmail.com>
2017-07-13 02:01:04 +0530

    LogOut Added

--- app.js
+++ app.js
@@ -30,9 +30,17 @@ app.get("/", function (req, res){
     res.render("home");
 }) ;
 
-app.get("/secret", function(req,res){
+app.get("/secret", isLoggedIn ,function(req,res){
         res.render("secret");
 });
+
+function isLoggedIn(req, res, next){
+        if (req. isAuthenticated())
+        {
+            return next();
+        }
+        res.redirect("/login");
+}
 
 app.get("/register", function(req, res){
     res.render("register");
@@ -61,6 +69,10 @@ app.post("/login", passport.authenticate("local", {
 }) , function(req, res){
 });
 
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+})
 app.listen(3000, function(){
     console.log("Server has started. ");
 })
